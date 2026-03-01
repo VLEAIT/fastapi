fa5ab87819d5353cdcae4338f1ff2497c1395be3
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-03-01 08:33:20 +0000

    students id

--- myapi.py
+++ myapi.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app =FastAPI()
 
-@app.get("/")
+students = {
+    1:{
+        "name":"hero",
+        "age":17,
+        "game":"chess"
+    }
+}
+   
 
+@app.get("/")
 def index():
     return("hero")
+
+
+@app.get("/get-hero/{student_id}")
+def get_herione(student_id: int):
+    return(students[student_id])
