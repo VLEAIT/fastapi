af0f196219fce8701d7714822d399e6df128dc81
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-03-05 04:35:45 +0000

    the put and delete

--- myapi.py
+++ myapi.py
@@ -17,6 +17,12 @@ class Student(BaseModel):
     age : int
     game : str
 
+class UpdateStudent(BaseModel):
+    name : Optional[str] =None
+    age : Optional[int] =None
+    game:Optional[str] =None
+
+
 @app.get("/")
 def index():
     return("hero")
@@ -40,3 +46,26 @@ def get_students(student_id:int, student:Student):
         return{"data":"exists"}
     students[student_id] = student
     return{students[student_id]}
+
+
+
+@app.put("/update_student/{student_id}")
+def update_student(*,student_id :int=Path(description="ID of student"),student:UpdateStudent):
+    if student_id not in students:
+       return{"error":"file doesnot exist"}
+    if student.name != None:
+        students[student_id].name =student.name
+    if student.age != None:
+        students[student_id].age =student.age
+    if student.game !=None:
+        students[student_id].game =student.game
+        
+    return students[student_id]
+
+@app.delete("/delete_student/{student_id}")
+def delete_student(student_id:int=Path(description="id of user to del")):
+    if student_id not in students:
+        return{"error":"the problem is you"}
+    
+    del students[student_id]
+    return("delted succesfully")
